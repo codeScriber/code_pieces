8e6d5c6de7f9b5e34c77f5eb7bf9bad11d93825b
eyal <eyal@rounds.com>
2016-12-14 13:27:43 +0200

    updating no-ip once again

--- node/no-ip-update/script.js
+++ node/no-ip-update/script.js
@@ -11,16 +11,15 @@ var argv = require('optimist')
     .argv;
 
 nightmare
+  .cookies.clearAll()
   .goto('https://my.noip.com/#!/dynamic-dns')
   .type('#clogs > input:nth-child(1)', argv.u)
   .type('#clogs > input:nth-child(2)', argv.p)
   .click('#clogs > button')
-  .wait('#content-wrapper > div.row > div.col-lg-8.col-md-8.col-sm-12 > div:nth-child(1) > div:nth-child(1) > div > div > div > div > div > span.text-bg.text-success')
-  .click('#content-wrapper > div.row > div.col-lg-8.col-md-8.col-sm-12 > div:nth-child(1) > div:nth-child(1) > div > div > div > div > div > span.text-bg.text-success')
+  .wait('#content-wrapper > div.row > div.col-lg-8.col-md-8.col-sm-12 > div:nth-child(1) > div:nth-child(1) > div > div > div > div > div > span.text-danger')
+  .click('#content-wrapper > div.row > div.col-lg-8.col-md-8.col-sm-12 > div:nth-child(1) > div:nth-child(1) > div > div > div > div > div > span.text-danger')
   .wait('#host-panel > table > tbody > tr > td.hidden-xs.clearfix > div')
   .click('#host-panel > table > tbody > tr > td.hidden-xs.clearfix > div')
-  .wait('#host-modal > div > div > div > div > div > form > div.panel-body > div.form-footer.text-right > span')
-  .click('#host-modal > div > div > div > div > div > form > div.panel-body > div.form-footer.text-right > span')
   .wait(3000)
   .end()
   .then(function () {
